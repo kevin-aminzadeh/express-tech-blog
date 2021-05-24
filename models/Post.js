41a99dfd3ce37bbb0e0da486ca97d176a9d89405
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-24 19:29:18 +0930

    feat: Implement Post model

---
+++ models/Post.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 100],
+      },
+    },
+    content: {
+      type: DataTypes.TEXT("long"),
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
