b900286c765d783e175431b95cd1b90ad04a4831
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-24 19:12:24 +0930

    feat: Implement User model hooks

--- models/User.js
+++ models/User.js
@@ -8,31 +8,57 @@ class User extends Model {
   }
 }
 
-User.init({
-  id: {
-    type: DataTypes.UUIDV4,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+User.init(
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 50],
+      },
     },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8, 50],
+  {
+    hooks: {
+      // Hash User Password On Create
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // Hash User Password On Update
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
     },
-  },
-});
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored, true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
