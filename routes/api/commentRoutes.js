fb578c8596b0db1bc9fae6fa2d78a91f856bd09a
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-25 20:07:18 +0930

    feat: Implement API routes for comments

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const { CommentController } = require("../../controllers");
 
-router.post("/", async (req, res) => {});
+// Create New Comment
+router.post("/", CommentController.createComment);
+
+// Update Existing Comment
+router.put("/", CommentController.updateComment);
+
+// Delete Comment
+router.delete("/:id", CommentController.deleteComment);
 
 module.exports = router;
