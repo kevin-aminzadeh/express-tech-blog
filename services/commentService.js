309ce6c8c3535412307783906401240fe2a4b2d2
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-25 20:06:19 +0930

    feat: Implement CommentService methods

--- services/commentService.js
+++ services/commentService.js
@@ -1,1 +1,44 @@
-// const { User, Post } = require("../models/index");
+const { Comment } = require("../models/index");
+
+// Create New Comment in DB
+exports.createComment = async (commentData) => {
+  try {
+    const dbData = await Comment.create(commentData);
+    return dbData;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Update Comment Record
+exports.updateComment = async (commentData) => {
+  try {
+    const res = await Comment.update(
+      { content: commentData.content },
+      {
+        where: {
+          id: commentData.id,
+          owner_id: commentData.ownerId,
+        },
+      }
+    );
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Delete Comment Record
+exports.updateComment = async (commentId, ownerId) => {
+  try {
+    const res = await Comment.destroy({
+      where: {
+        id: commentData.id,
+        owner_id: commentData.ownerId,
+      },
+    });
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
