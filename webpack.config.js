be5f6f2486e461ab0b4aa1b11ea63ad857b7258b
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-26 15:38:45 +0930

    chore: Implement Webpack + Babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const config = {
+  entry: "./public/assets/js/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
