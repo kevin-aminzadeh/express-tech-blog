a6ff0c3d423460637f50cfc6eba220155e865334
Kevin Aminzadeh <amin0015@flinders.edu.au>
2021-05-26 19:23:14 +0930

    chore: Implement Babel "plugin-transform-runtime" plugin

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const config = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
